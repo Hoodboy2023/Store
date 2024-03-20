6e25d2412dea09ce511fb1100b52f3a769c1843a
Hoodboy2023 <144848203+Hoodboy2023@users.noreply.github.com>
2024-03-20 20:23:50 +0000

    changes to routes.py

--- store_project/store/routes.py
+++ store_project/store/routes.py
@@ -59,7 +59,7 @@ def register_page():
         flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
         return redirect(url_for('market_page'))
     if form.errors != {}: #If there are not errors from the validations
-        for err_msg in form.errors.values:
+        for err_msg in form.errors.values():
             flash(f'There was an error with creating a user: {err_msg}', category='danger')
 
     return render_template('register.html', form=form)
