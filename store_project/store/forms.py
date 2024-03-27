f3689d82484286b43fe17f2f9f5c4cbf1eb74234
Hoodboy2023 <144848203+Hoodboy2023@users.noreply.github.com>
2024-03-27 18:39:37 +0000

    changes

--- store_project/store/forms.py
+++ store_project/store/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, HiddenField
+from wtforms import StringField, PasswordField, SubmitField, HiddenField, SelectField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError, Regexp
 from store.models import User
+from store.info import countries
 
 
 class RegisterForm(FlaskForm):
@@ -17,10 +18,20 @@ class RegisterForm(FlaskForm):
 
     username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email Address:', validators=[Email(message="Invalid Email address!!"), DataRequired()])
+    first_name = StringField(label='First Name',validators=[Length(min=2,max=15),DataRequired()])
+    last_name = StringField(label='Last Name',validators=[Length(min=2,max=15),DataRequired()])
+    phone_number = StringField(label='Phone Number', validators=[Length(max=15), DataRequired()])
+    country = SelectField(label='Country',choices=countries)
+    address1 = StringField(label='Address 1', validators=[Length(max=30), DataRequired()])
+    address2 = StringField(label='Address 2', validators=[Length(max=30), DataRequired()])
+    zip_address = StringField(label='Zip',validators=[DataRequired()])
+    city = StringField(label='City',validators=[Length(max=15),DataRequired()])
     password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
 
+   
+   
 
 class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
